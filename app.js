21b7ba167bae4ea876abe8bc9ea023eec036d699
mohammadhasan3 <mohamed.hassan940@hotmail.com>
2020-12-10 20:04:11 +0300

    Added sign up sign in and users list

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const db = require("./db/models");
+const passport = require("passport");
+const { localStrategy, jwtStrategy } = require("./api/middleware/passport");
 const cors = require("cors");
 const thingRoutes = require("./api/things/routes");
+const userRoutes = require("./api/users/routes");
 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(passport.initialize());
+passport.use(localStrategy);
+passport.use(jwtStrategy);
 
 // Routes
 app.use("/things", thingRoutes);
+app.use(userRoutes);
 
 // NOT FOUND PATH MIDDLEWARE
 app.use((req, res, next) => {
@@ -26,12 +33,12 @@ app.use((err, req, res, next) => {
 
 const run = async () => {
   try {
-    // await db.sequelize.sync();
-    await db.sequelize.sync({ alter: true });
+    await db.sequelize.sync();
+    // await db.sequelize.sync({ alter: true });
     // await db.sequelize.sync({ force: true });
     console.log("Connection to the database successful!");
-    app.listen(8001, () => {
-      console.log("The application is running on localhost:8001");
+    app.listen(8000, () => {
+      console.log("The application is running on localhost:8000");
     });
   } catch (error) {
     console.error("error", error);
